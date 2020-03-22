2e4c3ecaa5a8dddf3df50a6fab63f57e032e71c8
dgoldstein1 <david.goldstein@deciphernow.com>
2020-03-22 16:44:58 -0400

    refactor api declaration

--- backend/src/server.py
+++ backend/src/server.py
@@ -11,10 +11,10 @@ app.config.from_pyfile('../config.cfg')
 # db setup
 db = db.DB(os.environ['DYNAMO_DB_ENDPOINT'])
 
-
 ##########
 ## util ##
 ##########
+
 
 def serveDocs():
     """Serves docs to browser"""
@@ -22,35 +22,44 @@ def serveDocs():
 
 
 def errOut(code, error):
-	"""util function for returning non 2xx responses"""
-	logging.error(error)
-	return jsonify(code=code, error=error), code
+    """util function for returning non 2xx responses"""
+    logging.error(error)
+    return jsonify(code=code, error=error), code
+
 
 #########
 ## api ##
 #########
 
-# Navigator function
-app.add_url_rule('/navigator',
-                 "get navigator",
-                 navigatorHandlers.getNavigator,
-                 methods=["GET"])
-app.add_url_rule('/navigator',
-                 "add navigator",
-                 navigatorHandlers.postNavigator,
-                 methods=["POST"])
-app.add_url_rule('/navigator',
-                 "update navigator",
-                 navigatorHandlers.putNavigator,
-                 methods=["PUT"])
-app.add_url_rule('/navigator',
-                 "delete navigator",
-                 navigatorHandlers.deleteNavigator,
-                 methods=["DELETE"])
+supportedCrudEndpoints = [{
+    "name":
+    "navigator",
+    "path":
+    "/navigator",
+    "methods": [{
+        "method": "GET",
+        "handler": navigatorHandlers.getNavigator
+    }, {
+        "method": "POST",
+        "handler": navigatorHandlers.postNavigator
+    }, {
+        "method": "PUT",
+        "handler": navigatorHandlers.putNavigator
+    }, {
+        "method": "DELETE",
+        "handler": navigatorHandlers.deleteNavigator
+    }]
+}]
+
+for endpt in supportedCrudEndpoints:
+    for m in endpt.get("methods"):
+        app.add_url_rule(endpt.get("path"),
+                         "{} a {}".format(m.get("method"), endpt.get("name")),
+                         m.get("handler"),
+                         methods=[m.get("method")])
+
 # docs
 app.add_url_rule('/', "swagger docs", serveDocs)
 
-
-
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
---
+++ backend/watch_dev_changes.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+while true; do
+
+# setup testing configuration
+export GRAPH_SAVE_PATH="$(pwd)/out/test1.graph"
+
+inotifywait -e modify,create,delete -r ./ && \
+	clear && \
+	yapf -ri ./**/*.py && \
+	autoflake --in-place --remove-unused-variables ./**/*.py
+	# coverage run -m pytest
+done
