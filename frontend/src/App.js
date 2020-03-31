fd8eee81fc97a703f105ad22b244b0719e5ecb07
xzeroth <37749243+xzeroth@users.noreply.github.com>
2020-03-31 14:39:19 -0400

    added nav reg form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import LandlordRegisteration from "./components/LandlordRegistration";
-import "react-bulma-components/basic/react-bulma-components.min.css";
 import { NavRegForm } from "./components/NavRegForm";
 
 function App() {
--- frontend/src/components/NavRegForm.js
+++ frontend/src/components/NavRegForm.js
@@ -1,9 +1,66 @@
 import React from "react";
+import "bulma/css/bulma.css";
 
 export const NavRegForm = () => {
   return (
-    <div className="title">
-      <h1>Navigator Registeration</h1>
+    <div className="container">
+      <h1 className="title">Navigator Registeration</h1>
+      <form>
+        <div className="field">
+          <label className="label">First Name</label>
+          <div className="control">
+            <input
+              className="input is-expanded"
+              type="text"
+              placeholder="Text input"
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Last Name</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">User Name</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Password</label>
+          <div className="control">
+            <input className="input" type="password" placeholder="Password" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Email</label>
+          <div className="control">
+            <input className="input" type="email" placeholder="Text input" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Agency</label>
+          <div className="control">
+            <input className="input" type="text" placeholder="Text input" />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Phone Number</label>
+          <div className="control">
+            <input className="input" type="number" placeholder="Number input" />
+          </div>
+        </div>
+        <div className="field is-grouped">
+          <div className="control">
+            <button className="button is-link">Submit</button>
+          </div>
+          <div className="control">
+            <button className="button is-light">Cancel</button>
+          </div>
+        </div>
+      </form>
     </div>
   );
 };
