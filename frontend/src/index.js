eaabce6bf7ab730b3162b95c9fc41a08e0169e00
dgoldstein1 <david.goldstein@deciphernow.com>
2020-04-07 19:25:50 -0400

    added sample redux to projet

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import configureStore from './store';
+import App from './App';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+ <Provider store={configureStore()}>
+  <App />
+ </Provider>,
+ document.getElementById('root')
 );
---
+++ frontend/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from 'redux';
+import property from './property';
+export default combineReducers({
+ property
+});
