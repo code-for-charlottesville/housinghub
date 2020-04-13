3adc3cc3cd2d02889953cacf4ea73a15ed4c487e
MFineout <MdFineout@gmail.com>
2020-04-13 07:00:19 +0000

    refactor to allow data-binding

--- frontend/src/components/InputField.js
+++ frontend/src/components/InputField.js
@@ -1,16 +1,26 @@
 import React from "react";
 import store from "../reducers";
 
-export const InputField = ({ inputName, inputType, inputPh, inputValue, onChangeFn, onChangeFld }) => {
+export const InputField = ({
+  inputName,
+  inputType,
+  inputPh,
+  inputValue,
+  onChangeFn,
+  onChangeFld,
+}) => {
   return (
     <div className="field">
       <label className="label">{inputName}</label>
       <div className="control">
-        <input className="input" type={inputType} placeholder={inputPh} 
-               onChange={e => {
-                 store.dispatch(onChangeFn(onChangeFld, e.target.value))}
-               }
-               value={inputValue}
+        <input
+          className="input"
+          type={inputType}
+          placeholder={inputPh}
+          onChange={(e) => {
+            store.dispatch(onChangeFn(onChangeFld, e.target.value));
+          }}
+          value={inputValue}
         />
       </div>
     </div>
