54216b59bcfb2912fbaa3a14f95c9a432f3b5cd2
MFineout <MdFineout@gmail.com>
2020-04-13 07:02:07 +0000

    input component for number-validation

---
+++ frontend/src/components/InputFieldNum.js
@@ -0,0 +1,29 @@
+import React from "react";
+import store from "../reducers";
+
+export const InputFieldNum = ({
+  inputName,
+  inputType,
+  inputPh,
+  inputValue,
+  onChangeFn,
+  onChangeFld,
+}) => {
+  return (
+    <div className="field">
+      <label className="label">{inputName}</label>
+      <div className="control">
+        <input
+          className="input"
+          type={inputType}
+          placeholder={inputPh}
+          onChange={(e) => {
+            (e.target.value >= 0 &&
+            store.dispatch(onChangeFn(onChangeFld, e.target.value)))
+          }}
+          value={inputValue}
+        />
+      </div>
+    </div>
+  );
+};
