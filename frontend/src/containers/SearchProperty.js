d8cc9dbe2d58b7d9a0bd9cb9e542690a6f549dbe
dgoldstein1 <david.goldstein@deciphernow.com>
2020-04-18 14:01:12 -0400

    added SearchProperty file, removed DS store

---
+++ frontend/src/containers/SearchProperty.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const SearchProperty = (props) => {
+  return (
+  	<div>
+  	</div>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    
+  };
+}
+
+export default connect(mapStateToProps)(SearchProperty);
