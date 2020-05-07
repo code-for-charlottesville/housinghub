87be483139df805c8151f6d4cba68f7a74030e0e
vg5ak <vg5ak@virginia.edu>
2020-05-07 18:37:28 -0400

    dropdown.js
    ggg

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "bulma/css/bulma.css";
 import "../style/App.css";
 
 const Dropdown = (props) => {
+    let [name, getName] = useState('--');
+    let changeName = (i) => {
+        getName(i);
+    }
     return (
         <div class="dropdown is-hoverable">
           <div class="dropdown-trigger">
             <p>{props.getName}</p>
             <button class="button" aria-haspopup="true" aria-controls="dropdown-menu3">
-              <span>{props.getName}</span>
+              <span>{name}</span>
               <span class="icon is-small">
                 <i class="fas fa-angle-down" aria-hidden="true"></i>
               </span>
@@ -16,29 +20,32 @@ const Dropdown = (props) => {
           </div>
           <div class="dropdown-menu" id="dropdown-menu3" role="menu">
             <div class="dropdown-content">
-              <a href="#" class="dropdown-item">
+              <a href="#" class="dropdown-item" onClick={() => changeName(1)}>
                 1
               </a>
-              <a href="#" class="dropdown-item">
+              <a href="#" class="dropdown-item" onClick={() => changeName(2)}>
                 2
               </a>
-              <a href="#" class="dropdown-item">
+              <a href="#" class="dropdown-item" onClick={() => changeName(3)}>
                 3
               </a>
-              <a href="#" class="dropdown-item">
+              <a href="#" class="dropdown-item" onClick={() => changeName(4)}>
                 4
               </a>
-              <a href="#" class="dropdown-item">
+              <a href="#" class="dropdown-item" onClick={() => changeName(5)}>
                 5
               </a>
-              <a href="#" class="dropdown-item">
+              <a href="#" class="dropdown-item" onClick={() => changeName(6)}>
                 6
               </a>
-              <a href="#" class="dropdown-item">
+              <a href="#" class="dropdown-item" onClick={() => changeName(7)}>
                 7
               </a>
-              <a href="#" class="dropdown-item">
+              <a href="#" class="dropdown-item" onClick={() => changeName(8)}>
                 8
+              </a>
+              <a href="#" class="dropdown-item" onClick={() => changeName('--')}D>
+                "--"
               </a>
             </div>
           </div>
