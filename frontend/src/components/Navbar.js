0e12c889a315216f74406c48ef6fd7d124bee00f
MFineout <MdFineout@gmail.com>
2020-06-18 22:27:32 +0000

    Navbar looking close to Figma

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,16 +1,41 @@
 import React from "react";
+import "../style/App.css";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { setSidebarOpen } from "../actions/appState";
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 const LocalNavbar = (props) => {
   return (
-    <Navbar expand="true" bg="dark" variant="dark">
-      <Navbar.Toggle
-        aria-controls="responsive-navbar-nav"
-        onClick={() => props.dispatch(setSidebarOpen(!props.sidebarOpen))}
-      />
+    <Navbar expand="lg" bg="dark" variant="dark" className="container-fluid">
+      <Navbar.Brand href="/home">HousingHub</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link>
+            <Link to="./home" className="h-hub-nav-link">Home</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/property/search" className="h-hub-nav-link">Search</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/property/manage" className="h-hub-nav-link">My Houses</Link>
+          </Nav.Link>
+          <Nav.Link>
+            <Link to="/logout" className="h-hub-nav-link">Logout</Link>
+          </Nav.Link>
+          </Nav>
+          {/* <Link to="#about">About</Link> */}
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="lg-4 rounded-left" />
+          <Button variant="light">
+          <svg className="bi bi-search rounded-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+          </svg>
+          </Button>
+        </Form>
+      </Navbar.Collapse>
     </Navbar>
   );
 };
