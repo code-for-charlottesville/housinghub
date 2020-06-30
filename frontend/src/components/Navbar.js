000083c18cf6c1b111d1ff7502482133f17788e7
MFineout <MdFineout@gmail.com>
2020-06-30 22:04:36 +0000

    Use built in Bootstrap classes. update CSS.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -11,27 +11,21 @@ const LocalNavbar = (props) => {
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="flex-lg-fill">
           <Nav.Link>
-            <Link to="/property/search" className="h-hub-nav-link" title="Search property database">Search</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/property/manage" className="h-hub-nav-link" title="Manage properties">My Houses</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/about" className="h-hub-nav-link" title="Learn more about Code-For-Charlottesville!">About</Link>
-          </Nav.Link>
-          <Nav.Link>
-            <Link to="/logout" className="h-hub-nav-link">Logout</Link>
-          </Nav.Link> 
+            <Link to="/about" className="h-hub-nav-link text-white text-decoration-none" title="Learn more about Code-For-Charlottesville!">About</Link>
+          </Nav.Link>  
+          <Form className="flex-fill justify-content-lg-end" inline>
+            <FormControl type="text" placeholder="Search" className="col-10 search-input" />
+            <Button className="search-button" variant="light">
+            <svg className="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
+              <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
+            </svg>
+            </Button>
+            <Nav.Link>
+              <Link to="/logout" className="h-hub-nav-link text-white text-decoration-none">Logout</Link>
+            </Nav.Link> 
+          </Form> 
         </Nav> 
-        <Form className="flex-fill justify-content-lg-end" inline>
-          <FormControl type="text" placeholder="Search" className="col-10 search-input" />
-          <Button className="search-button" variant="light">
-          <svg className="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-            <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-            <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-          </svg>
-          </Button>
-        </Form>  
       </Navbar.Collapse>
     </Navbar>
   );
