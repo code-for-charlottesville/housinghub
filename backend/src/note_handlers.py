e44f6ece7937413bba0be00c4e8d5c9fc51158bc
Siyuan <39063901+ericssy@users.noreply.github.com>
2020-07-07 13:51:42 -0400

    Property & Note API (#78)
    * property model
    
    * property model
    
    * fixed typo
    
    * property services for search api and post property unit test
    
    * property service for search api
    
    * unit test
    
    * property service for search api and unit test
    
    * property service for search api and unit test
    
    * fixed search api and unit tests
    
    * search api and unit tests
    
    * git fetch upstream
    
    * added content and user id in Note Api
    
    * uid
    
    * uid
    
    * fix pytest
    
    * note api fix
    
    * note api
    
    * property handler
    
    * added update_note to API
    
    * property tests
    
    * blueprint

--- backend/src/note_handlers.py
+++ backend/src/note_handlers.py
@@ -8,14 +8,13 @@ from app.api import NoteSchema, AddNoteRequest, NoteResponse
 from app.auth import authenticate
 from app.spec import DocumentedBlueprint
 
-note_module = DocumentedBlueprint('note', __name__)
 
+note_module = DocumentedBlueprint('note', __name__)
 
 @note_module.route('/note', methods=['POST'])
 @authenticate
 def post_note():
     """
-
     adds a new Note to the database and returns response
     ---
     post:
@@ -44,10 +43,9 @@ def post_note():
                     application/json:
                         schema: ErrorResponse
     """
-
+    
     try:
-        payload = AddNoteRequest().load(request.get_json(force=True),
-                                        transient=True)
+        payload = AddNoteRequest().load(request.get_json(force = True), transient=True)
         _note = app.services.note_service().add_note(payload)
         return jsonify(NoteResponse().dump(_note))
     except ValidationError as err:
@@ -57,3 +55,49 @@ def post_note():
         app.logger.error(
             f'Unexpected error adding note: ${traceback.format_exc()}')
     return jsonify(code=500, error='internal error'), 500
+
+
+
+@note_module.route('/note', methods=['PUT'])
+@authenticate
+def put_note():
+    """updates a Property in the DB and returns the updated object
+    :param request: flask request object
+    :param request: dictionary of jwtPayload
+    :return tuple (response body (dict), response code (int), error (string))
+    """
+    try:
+        payload = request.get_json()
+        _note = app.services.note_service().update_note(payload)
+        return jsonify(NoteResponse().dump(_note))
+    except ValidationError as err:
+        app.logger.error(f'Invalid request ${err.messages}')
+        return jsonify(err.messages), 400
+    except:
+        app.logger.error(
+            f'Unexpected error adding property: ${traceback.format_exc()}')
+        return jsonify(code=500, error='internal error'), 500
+    return jsonify(code=500, error='not implemented'), 500
+
+
+
+@note_module.route('/note', methods=['DELETE'])
+@authenticate
+def delete_note():
+    """deletes a note in the DB and returns the deleted object
+    :param request: flask request object
+    :param request: dictionary of jwtPayload
+    :return tuple (response body (dict), response code (int), error (string))
+    """
+    try:
+        payload = request.get_json()
+        deleted_note = app.services.note_service().delete_note(payload)
+        return jsonify(NoteResponse().dump(deleted_note))
+    except ValidationError as err:
+        app.logger.error(f'Invalid request ${err.messages}')
+        return jsonify(err.messages), 400
+    except:
+        app.logger.error(
+            f'Unexpected error adding property: ${traceback.format_exc()}')
+        return jsonify(code=500, error='internal error'), 500
+    return jsonify(code=500, error='not implemented'), 500
