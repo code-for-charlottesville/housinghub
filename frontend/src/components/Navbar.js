7fa366fef738a524ced3bafe9147459a3e57126e
MFineout <MdFineout@gmail.com>
2020-07-07 22:05:38 +0000

    Styling. Convert user dropdown

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,34 +1,44 @@
 import React from "react";
+import { connect } from "react-redux";
 import "../style/App.css";
 import { Link } from "react-router-dom";
-import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import { Navbar, NavDropdown, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 const LocalNavbar = (props) => {
+
   return (
     <Navbar expand="lg" bg="dark" variant="dark" className="container-fluid">
-      <Navbar.Brand href="/home" className="h-hub-brand" title="Home">Housing Hub</Navbar.Brand>
+      <Navbar.Brand href="/home" title="Home">Housing Hub</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="flex-lg-fill">
           <Nav.Link>
-            <Link to="/about" className="h-hub-nav-link text-white text-decoration-none" title="Learn more about Code-For-Charlottesville!">About</Link>
+            <Link to="/about" className="text-white text-decoration-none h-hub-nav-link" title="Learn more about Code-For-Charlottesville!">About</Link>
           </Nav.Link>  
-          <Form className="flex-fill justify-content-lg-end" inline>
-            <FormControl type="text" placeholder="Search" className="col-10 search-input" />
+          <Form className="flex-fill justify-content-center" inline>
+            <FormControl id="nav-search" searchtype="text" placeholder="Search" className="col-6 search-input" />
             <Button className="search-button" variant="light">
             <svg className="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
               <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
             </svg>
             </Button>
-            <Nav.Link>
-              <Link to="/logout" className="h-hub-nav-link text-white text-decoration-none">Logout</Link>
-            </Nav.Link> 
-          </Form> 
+          </Form>
+          <NavDropdown title={props.username} id="Navbar-user-dropdown">
+              <NavDropdown.Item>
+                <Link to="/logout" className="text-info text-decoration-none">Logout</Link>
+              </NavDropdown.Item>
+          </NavDropdown> 
         </Nav> 
       </Navbar.Collapse>
     </Navbar>
   );
 };
 
-export default LocalNavbar;
+function mapStateToProps(state) {
+  return {
+    username: state.user.uid,
+  };
+}
+
+export default connect(mapStateToProps)(LocalNavbar);
