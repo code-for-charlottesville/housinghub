aa9aded620f0671a814072a70012e8d66390b216
MFineout <MdFineout@gmail.com>
2020-07-14 21:50:27 +0000

    Fixed CSS for expanded navbar. exported SVG.

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { connect } from "react-redux";
 import "../style/App.css";
+import BootstrapSearch from "../images/bootstrap-search";
 import { Link } from "react-router-dom";
 import { Navbar, NavDropdown, Nav, Form, FormControl, Button } from "react-bootstrap";
 
 const LocalNavbar = (props) => {
-
   return (
-    <Navbar expand="lg" bg="dark" variant="dark" className="container-fluid">
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="container-fluid">
       <Navbar.Brand href="/home" title="Home">Housing Hub</Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
@@ -15,13 +15,10 @@ const LocalNavbar = (props) => {
           <Nav.Link>
             <Link to="/about" className="text-white text-decoration-none h-hub-nav-link" title="Learn more about Code-For-Charlottesville!">About</Link>
           </Nav.Link>  
-          <Form className="flex-fill justify-content-center" inline>
-            <FormControl id="nav-search" searchtype="text" placeholder="Search" className="col-6 search-input" />
+          <Form className="flex-fill nav-toggle" inline>
+            <FormControl id="nav-search" searchtype="text" placeholder="Search" className="search-input" />
             <Button className="search-button" variant="light">
-            <svg className="bi bi-search" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-              <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 0 1 1.415 0l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1 0-1.415z"/>
-              <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
-            </svg>
+              <BootstrapSearch />
             </Button>
           </Form>
           <NavDropdown title={props.username} id="Navbar-user-dropdown">
