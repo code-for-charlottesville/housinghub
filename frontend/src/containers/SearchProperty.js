32cbbd922eaa331a61e958a3547684ce8c10aef9
dgoldstein1 <david.goldstein@deciphernow.com>
2020-07-28 13:06:50 -0400

    transitioned Search property to react class

--- frontend/src/containers/SearchProperty.js
+++ frontend/src/containers/SearchProperty.js
@@ -6,34 +6,36 @@ import SearchFiltersSidebar from "../components/SearchFiltersSidebar";
 import SearchTable from "../components/SearchTable";
 import { SEARCH_COLUMNS } from "../constants/global";
 
-const SearchProperty = (props) => {
-  //Populate rows
-  let rows = [];
-  for (let i = 0; i < props.searchResults.results.length; i++) {
-    let r = [
-      props.searchResults.results[i].address,
-      props.searchResults.results[i].property_name,
-      props.searchResults.results[i].monthly_rent,
-      props.searchResults.results[i].bedrooms,
-      props.searchResults.results[i].bathrooms,
-      <a href="">See More</a>,
-    ];
-    rows.push(r);
-  }
+class SearchProperty extends React.Component {
+  render() {
+    //Populate rows
+    let rows = [];
+    for (let i = 0; i < this.props.searchResults.results.length; i++) {
+      let r = [
+        this.props.searchResults.results[i].address,
+        this.props.searchResults.results[i].property_name,
+        this.props.searchResults.results[i].monthly_rent,
+        this.props.searchResults.results[i].bedrooms,
+        this.props.searchResults.results[i].bathrooms,
+        <a href="">See More</a>,
+      ];
+      rows.push(r);
+    }
 
-  return (
-    <div>
-      <Row className="search-page">
-        <Col xs={12} lg={3} xl={2} className="pr-0 border">
-          <SearchFiltersSidebar />
-        </Col>
-        <Col xs={12} lg={9} xl={10} className="pl-0 search-table">
-          <SearchTable rows={rows} columns={SEARCH_COLUMNS} />
-        </Col>
-      </Row>
-    </div>
-  );
-};
+    return (
+      <div>
+        <Row className="search-page">
+          <Col xs={12} lg={3} xl={2} className="pr-0 border">
+            <SearchFiltersSidebar />
+          </Col>
+          <Col xs={12} lg={9} xl={10} className="pl-0 search-table">
+            <SearchTable rows={rows} columns={SEARCH_COLUMNS} />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
 
 function mapStateToProps(state) {
   return {
