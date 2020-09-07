ff312e5c6380d6fedf30238bb7ca2439dda4af1c
Dan H <dan@MacBook-Pro.local>
2020-09-07 11:05:10 -0400

    Impl and docs for delete property API

--- backend/src/app/api.py
+++ backend/src/app/api.py
@@ -111,10 +111,3 @@ class UpdatePropertyResponse(SQLAlchemyAutoSchema):
     model = Property
     load_instance = True
 housinghub_spec.components.schema("UpdatePropertyResponse", schema=UpdatePropertyResponse)
-
-
-class DeletePropertyResponse(SQLAlchemyAutoSchema):
-  class Meta:
-    model = Property
-    load_instance = True
-housinghub_spec.components.schema("DeletePropertyResponse", schema=DeletePropertyResponse)
