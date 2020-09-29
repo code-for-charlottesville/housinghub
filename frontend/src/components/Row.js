fa2e24618217cc8f58b1d115ee485f46ad9dae9b
Yingqiao Xiong <yingqiaoxiong@Yingqiaos-MBP.local>
2020-09-29 15:43:13 -0400

    Clean Row.js

--- frontend/src/components/Row.js
+++ frontend/src/components/Row.js
@@ -3,11 +3,11 @@ import Cell from './Cell';
 
 
 // write a loop to list all the cells of the row
-const Row = (props) => { // Row needs selectedRowIndex, onRowSelect, columns
+const Row = (props) => { // Row needs selectedRowIndex, onRowSelect, columns, row, rowIndex
     return <tr
       className={props.selectedRowIndex === rowIndex ? "is-selected" : ""}
-      onClick={() => props.onRowSelect && props.onRowSelect(r, rowIndex)}
-      key={`row-${rowIndex}`}
+      onClick={() => props.onRowSelect && props.onRowSelect(props.row, props.rowIndex)}
+      key={`row-${props.rowIndex}`}
     >
       {props.columns.map((c, columnIndex) =>(
        <Cell value = {props.row[c.field]} rowIndex= {props.rowIndex} columnIndex= {columnIndex} />)
@@ -16,18 +16,4 @@ const Row = (props) => { // Row needs selectedRowIndex, onRowSelect, columns
   };
 
 // reference
-
-let _renderRow = (r, rowIndex) => (
-  <tr
-    className={props.selectedRowIndex === rowIndex ? "is-selected" : ""}
-    onClick={() => props.onRowSelect && props.onRowSelect(r, rowIndex)}
-    key={`row-${rowIndex}`}
-  >
-    {props.columns.map((c, columnIndex) =>
-      _renderCell(r[c.field], rowIndex, columnIndex)
-    )}
-  </tr>
-);
-
-
   export default Row;
