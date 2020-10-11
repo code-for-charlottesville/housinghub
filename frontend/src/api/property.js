f9b7a0a0bfab1f08282ccf15366641457dbd44f7
gmallari <gmallari@xforcefellow.us>
2020-10-11 12:41:47 -0400

    created delete and update funcitons

--- frontend/src/api/property.js
+++ frontend/src/api/property.js
@@ -17,3 +17,27 @@ export function postProperty(property) {
       return Promise.resolve(err.response.data);
     });
 }
+// Gabriel 
+export function updateProperty(propertyId, property) {
+  return axios
+    // post() 
+    .put(`${apiEndpoint}/property/${propertyId}`, property)
+    .then((r) => {
+      return Promise.resolve(r.data);
+    })
+    .catch((err) => {
+      return Promise.resolve(err.response.data);
+    });
+}
+
+export function deleteProperty(propertyId) {
+  return axios
+    // post() 
+    .delete(`${apiEndpoint}/property/${propertyId}`)
+    .then((r) => {
+      return Promise.resolve(r.data);
+    })
+    .catch((err) => {
+      return Promise.resolve(err.response.data);
+    });
+}
