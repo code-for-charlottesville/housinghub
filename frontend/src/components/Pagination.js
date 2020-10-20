e64c493fcfb01400faba87e88bf6d0bcccba08c6
April1029 <jj5rb@virginia.edu>
2020-10-20 18:01:59 -0400

    Show pagination correctly & Use primary button to indicate current page (#130)
    * clean up UX on Main page
    
    * Update Snapshot
    
    * Update Snapshot again
    
    * Show pagination correctly & Use primary button to indicate current page
    
    * Update snapshot

--- frontend/src/components/Pagination.js
+++ frontend/src/components/Pagination.js
@@ -6,28 +6,25 @@ import { ButtonGroup, Button } from "react-bootstrap";
 const Pagination = (props) => {
   let renderPaginationBox = (content, pageIndex) => (
     <Button
-      key={`pagination-box-${pageIndex}`}
+      key={`pagination-box-${content}`}
       className="PaginationBox"
-      variant="secondary"
+      variant={(pageIndex === props.currentPageIndex ? "primary" : "secondary")}
       onClick={() =>
         pageIndex !== props.currentPageIndex && props.onSetPage(pageIndex)
       }
     >
-      {pageIndex !== props.currentPageIndex && <a>{content}</a>}
-      {pageIndex === props.currentPageIndex && content}
+    {content === pageIndex ? pageIndex + 1 : content}
     </Button>
   );
 
-  let pagesToRender = props.pageIndexEnd - props.pageIndexStart;
+  let pagesToRender = props.numberOfPages;
   if (!pagesToRender) return <ul />;
   return (
     <ButtonGroup>
       {props.currentPageIndex !== 0 && renderPaginationBox("First", 0)}
+      {props.currentPageIndex !== 0 && renderPaginationBox("Prev", 0)}
       {_.times(pagesToRender, (i) =>
-        renderPaginationBox(
-          props.pageIndexStart + i + 1,
-          i + props.pageIndexStart
-        )
+        renderPaginationBox(i, i)
       )}
       {props.currentPageIndex !== props.numberOfPages - 1 &&
         renderPaginationBox("Next", props.currentPageIndex + 1)}
